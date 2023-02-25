4de760b62fa98f5c986ee51dff44d46ea45fd1f2
leodarkseid <leodarkseid@gmail.com>
2023-02-25 15:24:05 +0100

    first commit

--- tests/HelloWorld.ts
+++ tests/HelloWorld.ts
@@ -24,6 +24,12 @@ describe("Hello World!", () => {
     });
 
     it("should not allow anyone other than the owner to call transferownership",async () => {
-        helloWorldContract
-    })
+        const signers = await ethers.getSigners();
+        let owner = await helloWorldContract.owner();
+        console.log("The Owner before is " + owner);
+        const tx = await helloWorldContract.transferOwnership(signers[1].address);
+        await tx.wait();
+        owner = await helloWorldContract.owner();
+        console.log("The owner after is " + owner);
+    });
 });
