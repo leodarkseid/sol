937fdb259efb31f78cce69dec376becb75b3f238
leodarkseid <leodarkseid@gmail.com>
2023-02-25 12:23:39 +0100

    update

--- tests/HelloWorld.ts
+++ tests/HelloWorld.ts
@@ -1,11 +1,11 @@
-myFunction => (){
-    return true;
-}
+import { expect } from "chai";
+
+
 describe("My Title", () => {
     it("should work", () => {
         return true;
     })
     it("1 + 1 should equal to 2", () =>{
-        
+        expect(1 + 1).to.equal(2);
     })
 })
