b75c4f781b3ee7640e3cb092ab51e5f6d837e2a1
leodarkseid <leodarkseid@gmail.com>
2023-02-25 14:20:39 +0100

    update

--- tests/HelloWorld.ts
+++ tests/HelloWorld.ts
@@ -1,11 +1,11 @@
 import { expect } from "chai";
+import { ethers } from "hardhat";
 
 
-describe("My Title", () => {
-    it("should work", () => {
-        return true;
+describe("Hello World", () => {
+    it("should return Hello World!", async () => {
+         const helloWorldContracFactory = await ethers.getContractFactory("HelloWorld");
+         const helloWorldContract = await helloWorldContracFactory.deploy();
+         await helloWorldContract.deployed();
     })
-    it("1 + 1 should equal to 2", () =>{
-        expect(1 + 1).to.equal(2);
-    })
-})
+});
