a61c27fb49311f8082be6ae53df7654279378d5f
leodarkseid <leodarkseid@gmail.com>
2023-02-25 15:01:06 +0100

    first commit

--- tests/HelloWorld.ts
+++ tests/HelloWorld.ts
@@ -1,24 +1,25 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { HelloWorld } from "../typechain-types";
 
 
 describe("Hello World!", () => {
+    let helloWorldContract: HelloWorld;
+
+    beforeEach( async () => {
+        const helloWorldContractFactory = await ethers.getContractFactory("HelloWorld");
+        const helloWorldContract = await helloWorldContractFactory.deploy();
+        await helloWorldContract.deployed();
+    })
     it("should return Hello World!", async () => {
-         const helloWorldContracFactory = await ethers.getContractFactory("HelloWorld");
-         const helloWorldContract = await helloWorldContracFactory.deploy();
-         await helloWorldContract.deployed();
          const text = await helloWorldContract.helloWorld();
          expect(text).to.eq("Hello World!");
     });
 
     it("should set owner to deployer account", async() => {
-        const helloWorldContracFactory = await ethers.getContractFactory("HelloWorld");
-        const helloWorldContract = await helloWorldContracFactory.deploy();
-        await helloWorldContract.deployed();
         const signers = await ethers.getSigners();
         const deployerAccount = signers[0];
         const owner = await helloWorldContract.owner();
-        console.log(deployerAccount.address)
         expect(owner).to.eq(deployerAccount.address);
     });
 });
