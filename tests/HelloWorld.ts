2c939ea08ff9dcceedbc4b7c52c6ddd539415b87
leodarkseid <leodarkseid@gmail.com>
2023-02-25 12:14:52 +0100

    update

--- tests/HelloWorld.ts
+++ tests/HelloWorld.ts
@@ -1,1 +1,11 @@
- 
+myFunction => (){
+    return true;
+}
+describe("My Title", () => {
+    it("should work", () => {
+        return true;
+    })
+    it("1 + 1 should equal to 2", () =>{
+        
+    })
+})
